613964fdf8650f0ddeaff3014686a58ca764917c
mafuf42 <mafuf042@gmail.com>
2022-12-10 08:41:26 +0700

    fix initial data is None

--- hw1/cs285/infrastructure/rl_trainer.py
+++ hw1/cs285/infrastructure/rl_trainer.py
@@ -165,7 +165,7 @@ class RL_Trainer(object):
 
                 # (2) collect `self.params['batch_size']` transitions
 
-        if itr == 0:
+        if itr == 0 and load_initial_expertdata is not None:
             with open(load_initial_expertdata, "rb") as f:
                 loaded_paths = pickle.load(f)
             return loaded_paths, 0, None
